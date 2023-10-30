dfeff7af7373f73a82d9940e79165e550e031ffc
Sailok <127998789+Sailok25@users.noreply.github.com>
2023-10-30 23:48:09 +0100

    Delete JAVA/JAVA-main directory

--- JAVA/JAVA-main/examen/exercicis/src/exercici7.java
+++
@@ -1,8 +0,0 @@
-public class exercici7 {
-    public static void main(String[] args) {
-        /* Exercici 12 - Escriu un programa en Java que ens demani 2 nombres enters i un cop introduïts ens imprimeixi per pantalla el més gran d'aquests dos números.
-        Sí són iguals que ens imprimeixi: "Iguals!".
-         */
-        
-    }
-}
